519bfa256a1297f2b3f3b4f28aa9548fb7609fd2
Jonas Jacob Biermann <77678286+JonasBiermann@users.noreply.github.com>
2022-11-28 11:59:25 +0000

    stack task

--- cs_course/queue.py
+++ cs_course/queue.py
@@ -14,3 +14,39 @@ print(money)
 money.sort()
 
 print(money)
+
+# dishes
+t = [0, 1, 1, 1]
+b = [1, 0, 1, 1]
+
+if t[::-1] == b:
+    print('all good')
+
+# stack and queue for trains
+l1 = [5, 4, 3, 2, 1]
+l2 = [5, 3, 1, 2, 4]
+l3 = [4, 2, 5, 3, 1]
+l4 = [1, 4, 5, 3, 2]
+
+
+def stackMethod(list):
+    l1 = list
+    l0 = [1, 2, 3, 4, 5]
+    l2 = []
+    stackList = []
+    for i in range(len(list)):
+        if l0[i] == l1[i]:
+            l2.append(l0[i])
+        else:
+            stackList.append(l1[i])
+        if (stackList != [] and stackList[0] == l1[i+1]):
+            l2.append(stackList[0])
+            stackList = []
+        print('l2', l2)
+    if l2 == l1:
+        return True
+    else:
+        return False
+
+
+print(stackMethod(l3))
