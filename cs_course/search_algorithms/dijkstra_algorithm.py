33e5bc2a282a10e3955b9681c88ca74e8d4be7f2
Jonas Jacob Biermann <77678286+JonasBiermann@users.noreply.github.com>
2023-02-16 21:16:57 +0000

    tinkered around a bit with implementation - need to make sure that 1) only a shorter path is added and 2 once a shortest path is found the algorithm doesn't always go back to it but go to other nodes using queue and done

--- cs_course/search_algorithms/dijkstra_algorithm.py
+++ cs_course/search_algorithms/dijkstra_algorithm.py
@@ -3,8 +3,8 @@ u = float('inf')
 matrix = [[0, 2, 3, u, u, u, 7, u], [2, 0, u, 1, u, 5, 4, 1], [3, u, 0, u, 3, u, u, u], [u, 1, u, 0, u, 1, u, u], [
     u, u, 3, u, 0, 1, 1, u], [u, 5, u, 1, 1, 0, u, u], [7, 4, u, u, 1, u, 0, 2], [u, 1, u, u, u, u, 2, 0]]
 
-nodes = [[0, None], [u, None], [u, None], [u, None],
-         [u, None], [u, None], [u, None], [u, None]]
+nodes = [[0, None, True], [u, None, False], [u, None, False], [u, None, False],
+         [u, None, False], [u, None, False], [u, None, False], [u, None, False]]
 
 
 def findConnections(node):
@@ -23,14 +23,25 @@ def assignWeights(connections, node):
     return nodes
 
 
-print(assignWeights(findConnections(0), 0))
-
-
-def dijkstra(start):
-    while nodes[5][1] == None:
-        node = min(nodes[1:])
+def dijkstra():
+    while nodes[4][1] == None:
+        node = nodes.index(min(nodes))
+        print(node)
         assignWeights(findConnections(node), node)
         print(nodes)
 
 
-print(nodes)
+# dijkstra()
+
+node = nodes.index(min(nodes))
+print(node)
+print(assignWeights(findConnections(node), node))
+print('--------------')
+node = nodes.index(min(nodes[1:]))
+print(node)
+print(assignWeights(findConnections(node), node))
+print('--------------')
+node = nodes.index(min(nodes[1:]))
+print(node)
+print(assignWeights(findConnections(node), node))
+print('--------------')
