65930fd5e3c5c84167cda5f078b724e569fcae3f
Jonas Jacob Biermann <77678286+JonasBiermann@users.noreply.github.com>
2023-05-05 11:01:16 +0000

    start implementation rsa

---
+++ cs_course/encryption/rsa.py
@@ -0,0 +1,7 @@
+import math
+
+"""rsa functionality - https://www.javatpoint.com/rsa-encryption-algorithm
+
+implementation help - https://www.geeksforgeeks.org/rsa-algorithm-cryptography/
+
+generation of prime numbers - https://stackoverflow.com/questions/16091581/how-many-prime-numbers-are-there-available-for-rsa-encryption"""
