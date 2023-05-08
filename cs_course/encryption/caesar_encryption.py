788ac3aad31857d89bbd96df85f3e58e040ced17
Jonas Jacob Biermann <77678286+JonasBiermann@users.noreply.github.com>
2023-05-08 07:44:59 +0000

    worked on extra tasks for caesar encryption

--- cs_course/encryption/caesar_encryption.py
+++ cs_course/encryption/caesar_encryption.py
@@ -4,25 +4,25 @@ word = input('whats your word')
 alphabet = list(map(chr, range(97, 123)))
 
 
-def getPointer(_index, _key):
-    pointer = _index + _key
+def getPointer(index, key):
+    pointer = index + key
     if pointer > 26:
         pointer -= 26
     pointer -= 1
     return pointer
 
 
-def findChr(_c):
+def findChr(c):
     for value in alphabet:
-        if value[0] == _c:
+        if value[0] == c:
             return value[1]
     return None
 
 
-def encryptWord(_word, _key):
+def encryptWord(word, key):
     encryptedWord = ''
     for i in range(len(alphabet)):
-        pointer = getPointer(i, _key)
+        pointer = getPointer(i, key)
         alphabet[i] = [alphabet[i], pointer]
     print(alphabet)
 
@@ -31,6 +31,17 @@ def encryptWord(_word, _key):
 
     return encryptedWord
 
+def decryptWord(word, key):
+    decrypedWord = ''
+    for i in range(len(alphabet)):
+        pointer = getPointer(i, key)
+        alphabet[i] = [alphabet[i], pointer]
+    print(alphabet)
+    for c in word:
+        decrypedWord += alphabet[findChr(c)-key*2][0]
 
-for i in range(1, 26):
-    print(encryptWord(word, i+1))
+        
+
+print(decryptWord(word, key+1))
+
+
