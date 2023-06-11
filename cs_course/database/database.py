697da46e6260925049697d9a2fe4d52f9b289bba
Jonas Jacob Biermann <77678286+JonasBiermann@users.noreply.github.com>
2023-06-11 20:51:55 +0000

    finished gawo db

--- cs_course/database/database.py
+++ cs_course/database/database.py
@@ -6,57 +6,59 @@ cursor = connection.cursor()
 
 cursor.executescript(
     """
-    CREATE TABLE class 
+    CREATE TABLE classes 
 (
-    id	INT,
-    grade	INT
+    CId	INT,
+    grade	INT,
+    TId	INT
 );
 
-INSERT INTO class (id, grade) VALUES
-	('101', '10'),
-	('102', '10'),
-	('103', '10'),
-	('104', '10'),
-	('111', '11'),
-	('112', '11'),
-	('113', '11'),
-	('114', '11'),
-	('121', '12'),
-	('122', '12'),
-	('123', '12'),
-	('124', '12');
+INSERT INTO classes (CId, grade, TId) VALUES
+	('101', '10', '1'),
+	('102', '10', '2'),
+	('103', '10', '3'),
+	('104', '10', '4'),
+	('111', '11', '5'),
+	('112', '11', '6'),
+	('113', '11', '7'),
+	('114', '11', '8'),
+	('121', '12', '9'),
+	('122', '12', '10'),
+	('123', '12', '11'),
+	('124', '12', '12');
     """
 )
 
 cursor.executescript(
     """
-    CREATE TABLE student 
+    CREATE TABLE students 
 (
     SId	INT,
     firstname	VARCHAR(512),
     name	VARCHAR(512),
-    birthdate	DATE
+    birthdate	VARCHAR(512),
+    CId	INT
 );
 
-INSERT INTO student (SId, firstname, name, birthdate) VALUES
-	('1', 'William', 'Campbell', '2007-05-17'),
-	('2', 'Emma', 'Harris', '2007-03-28'),
-	('3', 'Isabella', 'Nelson', '2007-12-17'),
-	('4', 'Taylor', 'Martinez', '2007-05-25'),
-	('5', 'Alyssa', 'Moore', '2006-01-01'),
-	('6', 'Sofia', 'Lewis', '2006-02-28'),
-	('7', 'Melissa', 'Evans', '2006-04-11'),
-	('8', 'Joshua', 'Moore', '2006-09-23'),
-	('9', 'Haley', 'Brown', '2005-04-04'),
-	('10', 'Ava', 'Young', '2005-11-09'),
-	('11', 'Joshua', 'Moore', '2005-07-02'),
-	('12', 'Haley', 'Brown', '2005-10-03');
+INSERT INTO students (SId, firstname, name, birthdate, CId) VALUES
+	('1', 'William', 'Campbell', '2007-05-17', '101'),
+	('2', 'Emma', 'Harris', '2007-03-28', '101'),
+	('3', 'Isabella', 'Nelson', '2007-12-17', '103'),
+	('4', 'Taylor', 'Martinez', '2007-05-25', '102'),
+	('5', 'Alyssa', 'Moore', '2006-01-01', '104'),
+	('6', 'Sofia', 'Lewis', '2006-02-28', '111'),
+	('7', 'Melissa', 'Evans', '2006-04-11', '112'),
+	('8', 'Joshua', 'Moore', '2006-09-23', '112'),
+	('9', 'Haley', 'Brown', '2005-04-04', '113'),
+	('10', 'Ava', 'Young', '2005-11-09', '121'),
+	('11', 'Joshua', 'Moore', '2005-07-02', '122'),
+	('12', 'Haley', 'Brown', '2005-10-03', '123');
     """
 )
 
 cursor.executescript(
     """
-    CREATE TABLE teacher 
+    CREATE TABLE teachers
 (
     TId	INT,
     firstname	VARCHAR(512),
@@ -64,7 +66,7 @@ cursor.executescript(
     specialization	INT
 );
 
-INSERT INTO teacher (TId, firstname, lastname, specialization) VALUES
+INSERT INTO teachers (TId, firstname, lastname, specialization) VALUES
 	('1', 'William', 'Campbell', '1'),
 	('2', 'Emma', 'Harris', '4'),
 	('3', 'Isabella', 'Nelson', '3'),
@@ -82,14 +84,14 @@ INSERT INTO teacher (TId, firstname, lastname, specialization) VALUES
 
 cursor.executescript(
     """
-    CREATE TABLE room 
+    CREATE TABLE rooms
 (
     RId	VARCHAR(512),
     size	INT,
     type	VARCHAR(512)
 );
 
-INSERT INTO room (RId, size, type) VALUES
+INSERT INTO rooms (RId, size, type) VALUES
 	('UG1', '20', '1'),
 	('UG2', '21', '2'),
 	('UG3', '21', '1'),
@@ -142,23 +144,26 @@ INSERT INTO points (duration, points) VALUES
 
 cursor.executescript(
     """
-    CREATE TABLE presentation 
+    CREATE TABLE presentations 
 (
     PId	INT,
     title	VARCHAR(512),
     description	VARCHAR(512),
     starttime	VARCHAR(512),
     subject	VARCHAR(512),
-    date	DATE,
-    attendance	INT
+    date	VARCHAR(512),
+    attendance	INT,
+    TId	INT,
+    RId	VARCHAR(512),
+    duration	INT
 );
 
-INSERT INTO presentation (PId, title, description, starttime, subject, date, attendance) VALUES
-	('1', 'HTML', 'Lorem ipsum dolor sit amet.', '7,35', '1', '2023-03-27', '15'),
-	('2', 'Python', 'Lorem ipsum dolor sit amet.', '7,35', '1', '2023-03-29', '15'),
-	('3', 'Experimente', 'Lorem ipsum dolor sit amet.', '9,00', '2', '2023-03-28', '20'),
-	('4', '', 'Lorem ipsum dolor sit amet.', '10,15', '3', '2023-03-29', '25'),
-	('5', '', 'Lorem ipsum dolor sit amet.', '11,00', '4', '2023-03-30', '20');
+INSERT INTO presentations (PId, title, description, starttime, subject, date, attendance, TId, RId, duration) VALUES
+	('1', 'HTML', 'Lorem ipsum dolor sit amet.', '7,35', '1', '2023-03-27', '15', '1', 'OG10', '90'),
+	('2', 'Python', 'Lorem ipsum dolor sit amet.', '7,35', '1', '2023-03-29', '15', '3', 'EG7', '60'),
+	('3', 'Experimente', 'Lorem ipsum dolor sit amet.', '9,00', '2', '2023-03-28', '20', '5', 'OG2', '120'),
+	('4', 'USA', 'Lorem ipsum dolor sit amet.', '10,15', '3', '2023-03-29', '25', '7', 'UG3', '45'),
+	('5', 'Gedichte', 'Lorem ipsum dolor sit amet.', '11,00', '4', '2023-03-30', '20', '3', 'UG10', '90');
     """
 )
 
